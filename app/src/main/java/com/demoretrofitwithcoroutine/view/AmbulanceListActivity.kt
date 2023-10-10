b43186af2c10b5d5b122ecad4fcbc05a053c16f8
manendrasingh <manendra.singh@moveinsync.com>
2023-10-11 01:03:20 +0530

    Adding the extension function for the showing the toast message

--- app/src/main/java/com/demoretrofitwithcoroutine/view/AmbulanceListActivity.kt
+++ app/src/main/java/com/demoretrofitwithcoroutine/view/AmbulanceListActivity.kt
@@ -1,23 +1,19 @@
 package com.demoretrofitwithcoroutine.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.demoretrofitwithcoroutine.R
 import com.demoretrofitwithcoroutine.databinding.ActivityAmbulanceListBinding
 import com.demoretrofitwithcoroutine.view.ui.adapters.AmbulanceListAdapter
-import com.demoretrofitwithcoroutine.view.ui.adapters.HospitalsListAdapter
 import com.demoretrofitwithcoroutine.viewmodels.AmbulanceListViewModel
-import com.demoretrofitwithcoroutine.viewmodels.HospitalsListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class AmbulanceListActivity : AppCompatActivity() {
-    private val ambulanceViewModel: AmbulanceListViewModel by viewModels()
+    private val ambulanceViewModel: AmbulanceListViewModel by viewModels<AmbulanceListViewModel>()
      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityAmbulanceListBinding.inflate(layoutInflater)
--- app/src/main/java/com/demoretrofitwithcoroutine/view/ui/adapters/DoctorListAdapter.kt
+++ app/src/main/java/com/demoretrofitwithcoroutine/view/ui/adapters/DoctorListAdapter.kt
@@ -1,18 +1,22 @@
 package com.demoretrofitwithcoroutine.view.ui.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.demoretrofitwithcoroutine.R
 import com.demoretrofitwithcoroutine.databinding.ItemDoctorListBinding
 import com.demoretrofitwithcoroutine.model.NearDoctorList
 import com.demoretrofitwithcoroutine.model.SharedPreferenceHelper
-import dagger.hilt.android.AndroidEntryPoint
+import com.demoretrofitwithcoroutine.view.showMessage
 import javax.inject.Inject
 
 
 class DoctorListAdapter @Inject constructor(var sharedPreferenceHelper: SharedPreferenceHelper,var doctorList: List<NearDoctorList>) :
     RecyclerView.Adapter<DoctorListViewHolder>() {
+    lateinit var context: Context
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DoctorListViewHolder {
+        context = parent.context
         val inflater = LayoutInflater.from(parent.context)
         val binding = ItemDoctorListBinding.inflate(inflater, parent, false)
         return DoctorListViewHolder(binding)
@@ -30,6 +34,7 @@ class DoctorListAdapter @Inject constructor(var sharedPreferenceHelper: SharedPr
 
         with(holder.binding) {
             btnBookAppointment.setOnClickListener {
+                context.showMessage(context.getString(R.string.appointment_booked))
                 sharedPreferenceHelper.saveDoctorAppointment(doctorList[position])
             }
             with(doctorList[position]) {
