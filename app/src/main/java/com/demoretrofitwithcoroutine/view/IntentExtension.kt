9413f796b8dbedb4769ae4f5819ced8613553aac
manendrasingh <manendra.singh@moveinsync.com>
2023-10-11 02:04:55 +0530

    Creating Extension Function context and intent

--- app/src/main/java/com/demoretrofitwithcoroutine/view/IntentExtension.kt
+++ app/src/main/java/com/demoretrofitwithcoroutine/view/IntentExtension.kt
@@ -1,8 +1,25 @@
 package com.demoretrofitwithcoroutine.view
 
 import android.content.Context
+import android.content.Intent
 import android.widget.Toast
+
+//Extension function for showing a Toast message in the context.
 
 fun Context.showMessage(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 }
+
+//Extension function for obtaining an Intent for a specific class from a given context.
+
+fun Class<*>.getIntent(context: Context): Intent {
+    return Intent(context, this)
+}
+
+//Extension function for starting an activity for a given class in a specific context.
+
+fun Context.startActivity(cls: Class<*>) {
+    val intent = Intent(this, cls)
+    startActivity(intent)
+}
+
